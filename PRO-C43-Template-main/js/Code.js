def0eca55031408aa1d13917bef89af70960c547
Karmanya256 <73025872+Karmanya256@users.noreply.github.com>
2021-04-23 14:46:06 +0530

    Add files via upload

---
+++ PRO-C43-Template-main/js/Code.js
@@ -0,0 +1,25 @@
+const accessCode1 = "VARIABLE";
+const accessCode2 = "FUNCTION";
+const accessCode3 = "DATABASE";
+
+function clues() {
+    
+    fill("white")
+    textSize(15)
+    text("R E V B A I L A", 50,50)
+    fill("lightblue")
+    text("Hint: Always changing, not constant !!", 50,70)
+
+    fill("white")
+    textSize(15)
+    text("CUTNIFON", 350,50)
+    fill("lightblue")
+    
+    
+    
+    fill("white")
+    textSize(15)
+    text("ATADASBA", 700,50)
+    fill("lightblue")
+    
+}
